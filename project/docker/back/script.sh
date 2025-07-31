0daff8b88f25237b08aa54cb83083dd55a0d0072
EVBLOOD <saad.akllam@gmail.com>
2025-07-31 17:45:18 +0100

    basic backend starter with dockerfile added

--- project/docker/back/script.sh
+++ project/docker/back/script.sh
@@ -2,8 +2,8 @@ cd /var/www/html;
 
 python3.10 -m pip install --upgrade pip;
 
-python3.10 -m pip install --no-cache-dir --upgrade -r app/requirements.txt;
+python3.10 -m pip install --no-cache-dir --upgrade -r requirements.txt;
 
 echo "--------------------- Starting --------------------------------";
-tail -f /dev/null
-uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload
+
+uvicorn app:app --host 0.0.0.0 --port 8080 --reload
