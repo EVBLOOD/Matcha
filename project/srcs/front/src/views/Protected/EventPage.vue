e56363355f30182856f2430781ad94516f0af41a
Karim Idbouhouch <karim.idbouhouch@gmail.com>
2026-01-22 21:30:16 +0100

    Event page done

--- project/srcs/front/src/views/Protected/EventPage.vue
+++ project/srcs/front/src/views/Protected/EventPage.vue
@@ -67,15 +67,15 @@
                     </div>
                     <div class="status" v-if="event.type === 'sender'">
                         <span class="pending" v-if="event.status === 'pending'">
-                            <img src="/img/MessageEvent.svg" alt="pending" />
+                            <img src="/img/PendingEvent.svg" alt="pending" />
                             <p>Pending</p>
                         </span>
                         <span class="accepted" v-else-if="event.status === 'accepted'">
-                            <img src="/img/MessageEvent.svg" alt="pending" />
+                            <img src="/img/AcceptEventDark.svg" alt="pending" />
                             <p>Accepted</p>
                         </span>
                         <span class="declined" v-else-if="event.status === 'declined'">
-                            <img src="/img/MessageEvent.svg" alt="declined" />
+                            <img src="/img/DeclineEvent.svg" alt="declined" />
                             <p>Declined</p>
                         </span>
                     </div>
@@ -84,8 +84,14 @@
                             <img src="/img/DeclineEvent.svg" alt="decline" />
                         </button>
                         <button v-if="event.status === 'pending'" class="btn-accept">
-                            <img src="/img/AcceptEvent.svg" alt="accept" />
+                            <img src="/img/AcceptEventDark.svg" alt="accept" />
                         </button>
+                        <div class="status" v-if="event.status === 'declined'">
+                            <span class="declined" >
+                                <img src="/img/DeclineEvent.svg" alt="declined" />
+                                <p>Declined</p>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -94,144 +100,176 @@
 </template>
 
 <style lang="scss" scoped>
-    .page{
-        padding: 2%;
-        display: flex;
-        flex-direction: column;
-        gap: 0.5%;
-    }
-    
-    .content {
-        display: flex;
-        flex-direction: column;
-    }
+.page{
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5%;
+}
 
-    nav {
-        display: flex;
-        gap: 25px;
-        margin-bottom: 30px;
+.content {
+    display: flex;
+    flex-direction: column;
+}
+
+nav {
+    display: flex;
+    gap: 25px;
+    margin-bottom: 30px;
+}
+
+.tab{
+    width: 100%;
+}
+
+.link {
+    cursor: pointer;
+    text-decoration: none;
+    color: $text-color;
+    padding-bottom: 5px;
+    font-weight: 600;
+    &:hover {
+        border-style: solid;
+        border-width: 0px 0px 5px 0px;
+        border-color: $border-color-hover;
     }
-
-    .tab{
-        width: 100%;
+    &.active {
+        border-style: solid;
+        border-width: 0px 0px 5px 0px;
+        border-color: $border-color-active;
     }
+}
 
-    .link {
-        cursor: pointer;
-        text-decoration: none;
-        color: $text-color;
-        padding-bottom: 5px;
-        font-weight: 600;
-        &:hover {
-            border-style: solid;
-            border-width: 0px 0px 5px 0px;
-            border-color: $border-color-hover;
-        }
-        &.active {
-            border-style: solid;
-            border-width: 0px 0px 5px 0px;
-            border-color: $border-color-active;
-        }
-    }
+.events {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
 
-    .events {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-
-        .event-card {
-            background: $components-background-color;
-            padding: 3rem 2rem;
-            border-radius: 8px;
-            display: grid;
-            grid-template-columns: max-content 1fr min-content;
+    .event-card {
+        display: grid;
+        align-items: center;
+        grid-template-columns: minmax(200px, 250px) 1fr min-content;
+        background: $components-background-color;
+        padding: 3rem 2rem;
+        border-radius: 8px;
+        .users {
+            display: flex;
+            gap: 0.5rem;
             align-items: center;
-            // justify-content: space-between;
-
-            .users {
+            .user {
                 display: flex;
-                gap: 0.5rem;
+                flex-direction: column;
                 align-items: center;
-                
-
-                .user {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    gap: 0.25rem;
-
-                    img {
-                        border-radius: 50%;
-                    }
+                gap: 0.25rem;
+                img {
+                    border-radius: 50%;
                 }
             }
+        }
+        .details {
+            // flex: 1;
+            margin-left: 1rem;
+            display: flex;
+            gap: 40px;
+            // width: 100%;
+            .info {
+                display: flex;
+                align-items: center;
+                height: fit-content;
+                // width: auto;
+                gap: 8px;
+            }
 
+            .date_location{
+                display: flex;
+                flex-direction: column;
+                gap: 6px;
+            }
+        }
+
+        .status {
+            span {
+                white-space: nowrap;
+                padding: 10px 15px;
+                border-radius: 20px;
+                background: $components-background-color;
+                display: flex;
+                align-items: center;
+                gap: 5px;
+                font-weight: 600;
+            }
+            .accepted {
+                background: #e2bdf697;
+                color: #382343;
+            }
+            .pending {
+                color: #E2BDF6;
+            }
+            .declined {
+                color: #E2BDF6;
+            }
+        }
+
+        .confirmation {
+            display: flex;
+
+            button{
+                background: none;
+                border: none;
+                cursor: pointer;
+                margin-left: 10px;
+                border-radius: 10px;
+                height: 50px;
+                width: 50px;
+                transition: 0.3s;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .btn-accept {
+                background-color: #E2BDF6;
+            }
+            
+            .btn-decline {
+                background: $components-background-color;
+            }
+
+            button:hover {
+                filter: brightness(0.9);
+                transition: 0.3s;
+            }
+        }
+    }
+}
+
+@media (max-width: $breakpoint-md) {
+    .events {
+        .event-card {
+            grid-template-columns: 1fr;
+            gap: 1.5rem;
+            padding: 2rem 2rem;
+            font-size: 13px;
             .details {
-                // flex: 1;
-                margin-left: 1rem;
-                display: flex;
-                gap: 20%;
-                // width: 100%;
-                .info {
-                    display: flex;
-                    align-items: center;
-                    gap: 8px;
-                }
-
-                .date_location{
-
-                }
-
+                flex-direction: column;
+                margin-left: 0;
+                gap: 10px;
             }
-
             .status {
-                span {
-                    white-space: nowrap;
-                    padding: 10px 15px;
-                    border-radius: 20px;
-                    background: $components-background-color;
-                    display: flex;
-                    align-items: center;
-                    gap: 5px;
-                    font-weight: 600;
-                }
-                .accepted {
-                    background: #E2BDF6;
-                    color: #382343;
-                }
+                width: fit-content;
+                justify-content: flex-end;
             }
-
             .confirmation {
-                display: flex;
-
+                justify-content: flex-end;
+            }
+            .confirmation {
                 button{
-                    background: none;
-                    border: none;
-                    cursor: pointer;
-                    margin-left: 10px;
-                    border-radius: 10px;
-                    height: 60px;
-                    width: 60px;
-                    transition: 0.3s;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                .btn-accept {
-                    background-color: #BB85D7;
-                }
-                
-                .btn-decline {
-                    background: $components-background-color;
-                }
-
-                button:hover {
-                    filter: brightness(0.9);
-                    transition: 0.3s;
+                    height: 40px;
+                    width: 40px;
                 }
             }
         }
     }
+}
 
 </style>
