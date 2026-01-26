e1c5ce0f92c9c3d0bbe1a0e6e67f747443ef938f
Saad AKLLAM <saad.akllam@gmail.com>
2026-01-26 18:00:46 +0100

    make file and real prod mode in front

--- project/srcs/back/app/core/config.py
+++ project/srcs/back/app/core/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv ()
 
 class Config:
-    DB_HOST = os.getenv('DB_HOST', 'database')
+    DB_HOST = os.getenv('POSTGRES_HOST', 'database')
     DB_USER = os.getenv('POSTGRES_USER', 'postgres')
     DB_NAME = os.getenv('POSTGRES_DB', 'matcha')
     DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'No_PASS')
