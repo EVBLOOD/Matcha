4c19649647b7f9a9a138a37e7dee599a886556c9
Karim Idbouhouch <karim.idbouhouch@gmail.com>
2026-01-29 20:33:24 +0100

    fix toast content

--- project/srcs/front/src/components/ToastContainer.vue
+++ project/srcs/front/src/components/ToastContainer.vue
@@ -43,24 +43,89 @@ const go_to = (id: number | null, type: string) => {
   <div class="toast-container">
     <div v-for="toast in toasts" :key="toast.id" class="toast" :class="toast.type">
       <div class="left">
-        <div class="icon" :class="toast.type" >
-            <img :src="icon(toast.type)" alt="" />
+        <div class="header">
+          <div class="type">
+            <div class="icon" :class="toast.type" ><img :src="icon(toast.type)" alt="" /></div>
+            <p class="title" @click="go_to(toast.id_user, toast.type)">{{ toast.title }}</p>
+          </div>
+          <button class="close" @click="remove(toast.id)">✕</button>
         </div>
         <div class="content">
-            <h4 @click="go_to(toast.id_user, toast.type)">{{ toast.title }}</h4>
-            <div style="display: flex; align-items: center; gap: 3px;">
-              <img style="border-radius: 50%;" width="20px" height="20px" v-if="toast.avatar" :src="pictures_handler(toast.avatar)" alt="avatar">
-              <h6 v-if="toast.username">{{ toast.username }}</h6>
-              {{ toast.description }}
-            </div>
+          <p class="description">
+            <span class="user" v-if="toast.avatar || toast.username">
+              <span class="avatar" v-if="toast.avatar"><img :src="pictures_handler(toast.avatar)" alt="avatar" /></span>
+              <span class="username" v-if="toast.username">{{ toast.username }}</span>
+            </span>
+            {{ toast.description }}
+          </p>
         </div>
+
       </div>
-      <button class="close" @click="remove(toast.id)">✕</button>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.header{
+  display: flex;
+  justify-content: space-between;
+  .type{
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    .title{
+      font-size: 13px;
+      font-weight: 700;
+    }
+  }
+  .close {
+    background: transparent;
+    border: none;
+    font-size: 16px;
+    cursor: pointer;
+    color: #888;
+    padding: 4px;
+  }
+  .close:hover {
+    color: #000;
+  }
+}
+
+.content {
+  .description {
+    font-size: 13px;
+    line-height: 1.2;
+  }
+
+  .user {
+    display: inline-flex;
+    align-items: center;
+    gap: 4px;
+    font-weight: 600;
+    margin-right: 4px;
+    vertical-align: middle;
+  }
+
+  .avatar {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    overflow: hidden;
+    flex-shrink: 0;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      display: block;
+    }
+  }
+
+  .username {
+    white-space: nowrap;
+  }
+}
+
 .toast-container {
   position: fixed;
   top: 20px;
@@ -89,18 +154,21 @@ const go_to = (id: number | null, type: string) => {
 
 .toast .left {
   display: flex;
-  gap: 12px;
-  align-items: flex-start;
+  flex-direction: column;
+  gap: 5px;
+  width: 100%;
+  // align-items: flex-start;
 }
 
 .toast .icon {
     width: 25px;
     height: 25px;
+    min-width: 25px;
+    min-height: 25px;
     border-radius: 50%;
     display: flex;
     justify-content: center;
     align-items: center;
-
 }
 
 .icon.error { background-color: $toast-error; }
@@ -112,36 +180,9 @@ const go_to = (id: number | null, type: string) => {
 .icon.view { background-color: $toast-social; }
 
 .toast .icon img {
-    object-fit: contain;
-    width: 50%;
-    height: 50%;
-}
-
-.toast .content h4 {
-  font-size: 14px;
-  font-weight: 600;
-  margin: 0;
-  cursor: pointer;
-  align-items: center;
-}
-
-.toast .content p {
-  font-size: 13px;
-  margin: 4px 0 0;
-  color: #555;
-}
-
-.toast .close {
-  background: transparent;
-  border: none;
-  font-size: 16px;
-  cursor: pointer;
-  color: #888;
-  padding: 4px;
-}
-
-.toast .close:hover {
-  color: #000;
+  object-fit: contain;
+  width: 50%;
+  height: 50%;
 }
 
 .toast.error { border-bottom: 4px solid $toast-error; }
@@ -161,6 +202,43 @@ const go_to = (id: number | null, type: string) => {
     transform: translateX(0);
     opacity: 1;
   }
+}
+
+@media (max-width: $breakpoint-md) {
+  .header{
+    .type{
+      .title{
+        font-size: 12px;
+      }
+    }
+  }
+  .content {
+    .description {
+      font-size: 11px;
+      line-height: 0.9;
+    }
+    .user {
+      gap: 4px;
+      margin-right: 0;
+    }
+    .avatar {
+      width: 16px;
+      height: 16px;
+    }
+  }
+  .toast .icon {
+    width: 18px;
+    height: 18px;
+    min-width: 18px;
+    min-height: 18px;
+  }
+  .toast {
+    min-width: 240px;
+    max-width: 300px;
+    border-radius: 11px;
+    padding: 10px 12px;
+  }
+
 }
 
 </style>
